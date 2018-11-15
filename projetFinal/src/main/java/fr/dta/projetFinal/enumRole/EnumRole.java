5bc4afaadbd9229d0857f852ed1ef14a1bcd4e71
maxime TEYCHENEY <maxime.teycheney@gmail.com>
2018-11-15 10:54:32 +0100

    ajout de myUser et EnumRole

---
+++ projetFinal/src/main/java/fr/dta/projetFinal/enumRole/EnumRole.java
@@ -0,0 +1,23 @@
+package fr.dta.projetFinal.enumRole;
+
+public enum EnumRole {
+	ROLE_ADMIN("ROLE_ADMIN"),
+	ROLE_USER("ROLE_USER");
+	
+	private String role;
+	
+	private EnumRole(String s)
+	{
+		this.role = s;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
--- projetFinal/src/main/java/fr/dta/projetFinal/model/MyUser.java
+++ projetFinal/src/main/java/fr/dta/projetFinal/model/MyUser.java
@@ -2,17 +2,70 @@ package fr.dta.projetFinal.model;
 
 import javax.persistence.*;
 
+import fr.dta.projetFinal.enumRole.EnumRole;
+
 @Entity
-public class MyUser {
+public class MyUser
+{
 	@Id
 	@GeneratedValue
 	private long id;
 	
 	@Column
 	private String login;
+
+	@Column
+	private String password;
+	
+
+	@Enumerated(EnumType.STRING)
+	@Column
+	private EnumRole role;
+	
 	
 	public MyUser()
 	{
-		
+		this("login", "password");
 	}
+	
+	public MyUser(String login, String password)
+	{
+		this.login = login;
+		this.password = password;
+		this.role = EnumRole.ROLE_USER;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public EnumRole getRole() {
+		return role;
+	}
+
+	public void setRole(EnumRole role) {
+		this.role = role;
+	}
+	
+	
 }
