9d10dc779c53284c8b379f776c77cb7731cc436b
jonathan merlin <jo_merlin@hotmail.fr>
2018-11-15 11:06:04 +0100

    ajout de spring security

---
+++ projetFinal/src/main/java/fr/dta/projetFinal/security/AuthentificationService.java
@@ -0,0 +1,50 @@
+package fr.dta.projetFinal.security;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.dta.projetFinal.enumRole.EnumRole;
+import fr.dta.projetFinal.model.MyUser;
+import fr.dta.projetFinal.service.MyUserService;
+
+@Service
+@Transactional
+public class AuthentificationService implements UserDetailsService{
+
+	@Autowired
+	private MyUserService myuserservice;
+	
+	@Override
+	public UserDetails loadUserByUsername(final String username) {
+		Optional<MyUser> option = myuserservice.findOneByLogin(username);
+		if (option.isPresent()) {
+			MyUser user = option.get();
+			List<GrantedAuthority> rules = this.getUserCredentials(user);
+			return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), rules);
+		}
+		throw new UsernameNotFoundException("username.not.found");
+	}
+
+	private List<GrantedAuthority> getUserCredentials(MyUser user) {
+		 List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		 
+		 authorities.add(new SimpleGrantedAuthority(EnumRole.ROLE_USER.getRole()));
+		 if(user.getRole().equals(EnumRole.ROLE_ADMIN.getRole()))
+		 {
+			 authorities.add(new SimpleGrantedAuthority(EnumRole.ROLE_ADMIN.getRole()));
+		 }
+		 
+		return authorities;
+	}
+
+}
---
+++ projetFinal/src/main/java/fr/dta/projetFinal/service/MyUserService.java
@@ -0,0 +1,17 @@
+package fr.dta.projetFinal.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import fr.dta.projetFinal.model.MyUser;
+
+@Service
+public class MyUserService {
+
+	// a implémenter
+	public Optional<MyUser> findOneByLogin(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
