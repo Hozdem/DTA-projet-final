44d1f58bf4f2c5ed1de5a08f2516a205154c7e06
jonathan merlin <jo_merlin@hotmail.fr>
2018-11-16 12:29:52 +0100

    ajout de l'interface graphique du formulaire dajout dutilisateur

--- projetFinal/Front/projetFinalFront/src/app/user-reactive-form/user-reactive-form.component.ts
+++ projetFinal/Front/projetFinalFront/src/app/user-reactive-form/user-reactive-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { MyUser } from '../my-user';
 import { UserService } from '../user.service';
 import {SelectItem} from 'primeng/api';
+import { Password } from 'primeng/password';
+import { EmailValidator } from '@angular/forms'; 
 
 
 interface Role {
@@ -16,11 +18,14 @@ interface Role {
 })
 
 
+
+
 export class UserReactiveFormComponent implements OnInit {
   userForm = this.fb.group({
     login: ['', Validators.required],
     password: ['', Validators.required],
-		role: [{name: ""}, Validators.required],
+    verificationPassword: [''],
+		role: [{name: "ROLE_USER"}],
 		nom: ['', Validators.required],
 		prenom: ['', Validators.required],
 		email: ['', Validators.required],
@@ -28,28 +33,45 @@ export class UserReactiveFormComponent implements OnInit {
 		ville: ['', Validators.required],
 		codePostal: ['', Validators.required],
 		numTel: ['']
-  });
-
+  }, {validator: this.verifierPassword('password', 'verificationPassword')});
   
-
+  
+  
   roles : SelectItem[];
-
+  
   constructor(private fb: FormBuilder, private serviceUser: UserService) { 
     this.roles = [
       {label: "ROLE_USER", value: {id: 1, name:"ROLE_USER"}},
       {label: "ROLE_ADMIN", value: {id: 2, name:"ROLE_ADMIN"}}
     ];
   }
-
+  
   ngOnInit() {
   }
-
+  
   onSubmit()
   {
-        // en cas d'ajout
-    let user = new MyUser(this.userForm.value.login, this.userForm.value.password, this.userForm.value.role, this.userForm.value.nom
-          , this.userForm.value.prenom, this.userForm.value.email, this.userForm.value.adresse, this.userForm.value.ville
-          , this.userForm.value.codePostal, this.userForm.value.numTel);
-    this.serviceUser.addUser(user);
+      // en cas d'ajout
+      let leRole = this.userForm.value.role.name;
+      console.log(leRole);
+      let user = new MyUser(this.userForm.value.login, this.userForm.value.password, leRole, this.userForm.value.nom
+        , this.userForm.value.prenom, this.userForm.value.email, this.userForm.value.adresse, this.userForm.value.ville
+        , this.userForm.value.codePostal, this.userForm.value.numTel);
+        this.serviceUser.addUser(user);
+  }
+
+  verifierPassword(passwordKey: string, passwordConfirmationKey: string)
+  {
+    console.log('entree');
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey],
+          passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+          return passwordConfirmationInput.setErrors(null);
+      }
+    }
   }
 }
