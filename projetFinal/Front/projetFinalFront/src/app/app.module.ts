594f54922bbeeef2086d808cababb580e214efd1
hozdem <thiebaut.quentin@gmail.com>
2018-11-17 17:54:59 +0100

    rien

--- projetFinal/Front/projetFinalFront/src/app/app.module.ts
+++ projetFinal/Front/projetFinalFront/src/app/app.module.ts
@@ -10,32 +10,32 @@ import { RouterModule, Routes, Router} from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 
 // primeNG
 import {AccordionModule} from 'primeng/accordion';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {PasswordModule} from 'primeng/password';
 import {InputMaskModule} from 'primeng/inputmask';
-import { ProduitComponent } from './produit/produit.component';
 import {DropdownModule} from 'primeng/dropdown';
 import {TabMenuModule} from 'primeng/tabmenu';
 import {PanelMenuModule} from 'primeng/panelmenu';
-import { ConnexionReactiveFormComponent } from './connexion-reactive-form/connexion-reactive-form.component';
 import {ButtonModule} from 'primeng/button';
 
 import { AddUserReactiveFormComponent } from './add-user-reactive-form/add-user-reactive-form.component';
 import { UpdateUserReactiveFormComponent } from './update-user-reactive-form/update-user-reactive-form.component';
 import { ProduitDetailsComponent } from './produit-details/produit-details.component';
 import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { ConnexionReactiveFormComponent } from './connexion-reactive-form/connexion-reactive-form.component';
+import { ProduitComponent } from './produit/produit.component';
 
 const route: Routes = 
 [
   {path: 'connexion', component: ConnexionReactiveFormComponent},
   {path: 'addUser', component: AddUserReactiveFormComponent},
   {path: 'updateUser/:id', component: UpdateUserReactiveFormComponent},
-  {path: 'deleteUser/:id', component: DeleteUserComponent}
+  {path: 'deleteUser/:id', component: DeleteUserComponent},
+  {path: 'produit', component: ProduitDetailsComponent}
 ];
 
 @NgModule({
@@ -45,7 +45,9 @@ const route: Routes =
     AddUserReactiveFormComponent,
     UpdateUserReactiveFormComponent,
     ProduitDetailsComponent,
-    DeleteUserComponent
+    DeleteUserComponent,
+    ProduitComponent,
+    ProduitDetailsComponent
   ],
   imports: [
     BrowserModule,
