d659502b4f562444096d8f0e961f0c13c80b094e
hozdem <thiebaut.quentin@gmail.com>
2018-11-20 14:25:15 +0100

    criteria

--- projetFinal/src/main/java/fr/dta/projetFinal/repository/ProduitRepositoryImpl.java
+++ projetFinal/src/main/java/fr/dta/projetFinal/repository/ProduitRepositoryImpl.java
@@ -1,12 +1,13 @@
 package fr.dta.projetFinal.repository;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,36 +18,44 @@ import fr.dta.projetFinal.model.Produit;
 
 public class ProduitRepositoryImpl implements ProduitRepositoryCustom {
 
-private EntityManager em;
-	
+	private EntityManager em;
+
 	@Override
 	public List<Produit> search(String titre, List<String> genres, List<String> supports) {
 		CriteriaBuilder builder = em.getCriteriaBuilder();
 		CriteriaQuery<Produit> query = builder.createQuery(Produit.class);
 		Root<Produit> root = query.from(Produit.class);
-		
+
 		if (titre != null && !StringUtils.isEmpty(titre)) {
-			query.where(builder.like(root.get("titre"), "%" + titre + "%"));
+			query.where(builder.like(builder.upper(root.get("titre")), "%" + titre.toUpperCase() + "%"));
 		}
-		if (genres !=null && !genres.isEmpty()) {
-			for(String ge : genres) {
-				query.where(builder.like(root.get("genres"), "%" + ge + "%"));
+
+		if (genres != null && !genres.isEmpty()) {
+			List<Predicate> genresRestriction = new ArrayList<>();
+			for (String genre : genres) {
+				genresRestriction.add(builder.isMember(genre, root.get("genres")));
 			}
+			query.where(builder.or(genresRestriction.toArray(new Predicate[genresRestriction.size()])));
 		}
-		if (supports !=null && !supports.isEmpty()) {
-			for(String su : supports) {
-				query.where(builder.like(root.get("supports"), "%" + su + "%"));
+
+		if (supports != null && !supports.isEmpty()) {
+			List<Predicate> supportsPredicate = new ArrayList<>();
+
+			for (String support : supports) {
+				supportsPredicate.add(builder.like(root.get("support"),support));
 			}
+			query.where(builder.or(supportsPredicate.toArray(new Predicate[supportsPredicate.size()])));
 		}
-		if(titre != null && !StringUtils.isEmpty(titre) && genres !=null && !genres.isEmpty()) {
+
+		if (titre != null && !StringUtils.isEmpty(titre) && genres != null && !genres.isEmpty()) {
 			query.select(query.from(Produit.class));
 		}
-		
+
 		TypedQuery<Produit> userQuery = em.createQuery(query);
-		
+
 		return userQuery.getResultList();
 	}
-	
+
 	@Autowired
 	public void setJpaContext(JpaContext jpaContext) {
 		em = jpaContext.getEntityManagerByManagedType(Produit.class);
