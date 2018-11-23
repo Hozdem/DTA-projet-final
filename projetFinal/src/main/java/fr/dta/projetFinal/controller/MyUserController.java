1b69c4809a7b93084f21ec2e4e18436e96bfd1c8
jonathan merlin <jo_merlin@hotmail.fr>
2018-11-23 12:09:13 +0100

    correction dun bug eclipse avec project>clean

--- projetFinal/src/main/java/fr/dta/projetFinal/controller/MyUserController.java
+++ projetFinal/src/main/java/fr/dta/projetFinal/controller/MyUserController.java
@@ -1,22 +1,17 @@
 package fr.dta.projetFinal.controller;
 
-import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.dta.projetFinal.model.MyUser;
 import fr.dta.projetFinal.service.MyUserService;
